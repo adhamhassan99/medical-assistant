d9fc4d61cd22cf8831100f178e340830af45b342
Adham <adhamhassan100@yahoo.com>
2025-06-25 19:58:45 +0300

    keyboard avoiding view added

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { Button, createTamagui, ScrollView, TamaguiProvider, YStack } from 'tamagui';
 import { Send } from '@tamagui/lucide-icons'
 
@@ -18,14 +18,18 @@ export default function App() {
       <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
         <View style={styles.container}>
           <Header />
-          <ScrollView showsVerticalScrollIndicator={false} width={'100%'} marginBottom={20}>
+          <KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
 
-            <YStack paddingHorizontal={20} gap={20} width={'100%'}>
-              <MoodSelector />
-              <SleepTimePicker />
-              <OptionalNotes />
-            </YStack>
-          </ScrollView>
+            <ScrollView showsVerticalScrollIndicator={false} width={'100%'} marginBottom={20}>
+
+              <YStack paddingHorizontal={20} gap={20} width={'100%'}>
+                <MoodSelector />
+                <SleepTimePicker />
+                <OptionalNotes />
+              </YStack>
+            </ScrollView>
+          </KeyboardAvoidingView>
+
           <Button width={'90%'} icon={Send} backgroundColor={'#0280f6'} color={'#ffffff'} fontWeight={'bold'} size={'$4'}>
             Submit
           </Button>
